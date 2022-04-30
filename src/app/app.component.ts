7e436c54d67ab328121d0d5a1a97b3d34f5007de
podobar <robert.podoba73@gmail.com>
2022-04-30 10:20:44 +0200

    Initial commit: full initial setup of solution complete (api and front-end are integrated)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
+  public webApiUrl=""
+  public forecasts?: WeatherForecast[];
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('/weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+
   title = 'CourseApp';
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
 }
